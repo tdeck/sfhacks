81693556c7da6849643c2f8245a20984c8e586b1
Troy H. Deck <troy.deque@gmail.com>
2016-04-20 00:02:01 -0700

    Add alert email text for next 7 days

--- app/jobs/mail_alerter_job.rb
+++ app/jobs/mail_alerter_job.rb
@@ -2,12 +2,15 @@ class MailAlerterJob < ActiveJob::Base
   queue_as :default
 
   def perform(*args)
-    count = Lead.pending.where('date < ?', Date.today + 30.days).count
-    if count > 0
+    count30 = Lead.pending.where('date < ?', Date.today + 30.days).count
+    if count30 > 0
+      count7 = Lead.pending.where('date < ?', Date.today + 7.days).count
+      text = "There are #{count30} un-reviewed leads on sfhacks.com for events in the next 30 days."
+      text += "\nThere are #{count7} within the next 7 days!" if count7 > 0
       RestClient.post(
         'https://varmail.me/d8NWC6f8S9SR',
         subject: 'New event leads to review',
-        text: "There are #{count} un-reviewed leads on sfhacks.com for events in the next 30 days."
+        text: text
       )
     end
   end
